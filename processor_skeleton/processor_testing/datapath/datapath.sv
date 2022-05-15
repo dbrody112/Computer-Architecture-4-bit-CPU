6b2fe6e7bf51489d15f8ff88d0296190378fd78f
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2022-05-15 12:29:09 -0400

    edit datapath

--- processor_skeleton/processor_testing/datapath/datapath.sv
+++ processor_skeleton/processor_testing/datapath/datapath.sv
@@ -21,26 +21,28 @@ module datapath #(parameter DWIDTH = 8, IWIDTH = 16)(
   logic [DWIDTH-1:0] result;
   logic [DWIDTH-1:0] addTwoBytesToPC = {DWIDTH{1'b0}} + 2; 
 
-  // next PC logic
+  
+  //alu logic
 
-  mux2 wrmux(instr[7:6], instr[9:8], regdst,writereg); //set write for I-type or R-type
+  mux2 wrmux(instr[7:6], instr[9:8], regdst,writereg); //set write for R-type or I-type (regdst true for r)
   regfile rf(clk, regwrite, instr[11:10],instr[9:8],writereg, result, srca, writedata);
-  mux2 resmux(aluout,readdata,memtoreg,result);
+  mux2 resmux(readdata,aluout,memtoreg,result);
   signext se(instr[15:0],signimm); //I or R
 
-  mux2 srcbmux (writedata,signimm,alusrc,srcb); //I or R
+  mux2 srcbmux (signimm,writedata,alusrc,srcb); //I or R
   alu alu(srca,srcab,alucontrol, aluout,zero);
 
   pcsrc = branch & zero;
 
+  // next PC logic
 
   flopr #(DWIDTH) pcreg(clk, reset, pcnext, pc);
   adder #(DWIDTH) pcadd1(pc, addTwoBytesToPC, pcplus2);
   
   sl2   #(DWIDTH) immsh(signimm, signimmsh);
   adder #(DWIDTH) pcadd2(pcplus2, signimmsh, pcbranch);
-  mux2  #(DWIDTH) pcbrmux(pcplus2, pcbranch, pcsrc, pcnextbr);
-  mux2  #(DWIDTH) pcmux(pcnextbr, {instr[7:0]}, jump, pcnext);
+  mux2  #(DWIDTH) pcbrmux(pcbranch,pcplus2, pcsrc, pcnextbr);
+  mux2  #(DWIDTH) pcmux({instr[11:0]}, pcnextbr, jump, pcnext);//jump
   // instr[7:0] according to ISA should be instr[11:0] for the 13-bit jump address BUT
   // instr[11:0] means 12-bit address to do an unconditional jump; PC = jump address
   // FYI we do not have enough memory with an 8-bit CPU with byte addressing to use this.
