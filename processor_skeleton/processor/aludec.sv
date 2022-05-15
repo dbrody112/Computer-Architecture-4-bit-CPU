4a5a89a78ded0b07b59e2cf942ff9b47eea2af8a
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2022-05-15 12:12:55 -0400

    complete decoder

--- processor_skeleton/processor/aludec.sv
+++ processor_skeleton/processor/aludec.sv
@@ -2,28 +2,28 @@
 
 //R-Type
 
-`define AND  4'd0  
-`define OR  4'd1  
-`define NOR 4'd2  
-`define ADD   4'd3  
-`define SUB   4'd4  
-`define MOVZ   4'd5  
-`define LSL  4'd6  
-`define LSR  4'd7  
-`define DIV   4'd8  
-`define SLT   4'd9  
+`define AND  4'b0000  
+`define OR  4'b0001  
+`define NOR 4'b0010  
+`define ADD   4'b0011  
+`define SUB   4'b0100  
+`define XOR   4'b0101  
+`define LSL  4'b0110  
+`define LSR  4'b0111  
+`define DIV   4'b1000 
+`define SLT   4'b1001 
 
 //I-type
 
-`define LOAD   4'd10 
-`define STORE  4'd11 
-`define ADDI  4'd12 
-`define SUBI  4'd13 
-`define BEQ   4'd14 
+`define LOAD   4'b1010 
+`define STORE  4'b1011 
+`define ADDI  4'b1100 
+`define SUBI  4'b1101 
+`define BEQ   4'b1110 
 
 // J-Type
 
-`define B     4'd15 
+`define B     4'b1111 
 
 module aludec(input logic [3:0] op,
               input logic [1:0] aluop,
@@ -31,8 +31,9 @@ module aludec(input logic [3:0] op,
     always @*
     case(aluop)
         2'b00 : case(op)
-                    `ADDI : alucontrol <= 3'b000;
-                    `SUBI : alucontrol <= 3'b001;
+                    `ADDI : alucontrol <= 4'b0000;
+                    `SUBI : alucontrol <= 4'b0001;
+                    default: alucontrol <= 4'bxxxx;
         endcase
         2'b01 : alucontrol <= 3'b001; //sub for BEQ
         default: case(op)
@@ -40,13 +41,12 @@ module aludec(input logic [3:0] op,
             `SUB : alucontrol <=4'b0001;
             `AND : alucontrol <=4'b0010;
             `OR : alucontrol <= 4'b0011;
-            `NOR : alucontrol <= 4'b0010;
-            `MOVZ : alucontrol <= 4'b0100;
-            `DIV : alucontrol <= 4'b0101;
-            `SLT :alucontrol <= 4'b0110;
+            `NOR : alucontrol <= 4'b0100;
+            `XOR : alucontrol <= 4'b0101;
+            `DIV : alucontrol <= 4'b0110;
+            `SLT :alucontrol <= 4'b0111;
             `LSL : alucontrol <= 4'b1000;
             `LSR : alucontrol <= 4'b1001;
         endcase
     endcase
 endmodule
-
--- processor_skeleton/processor/maindec.sv
+++ processor_skeleton/processor/maindec.sv
@@ -2,28 +2,28 @@
 
 //R-Type
 
-`define AND  4'd0  
-`define OR  4'd1  
-`define NOR 4'd2  
-`define ADD   4'd3  
-`define SUB   4'd4  
-`define MOVZ   4'd5  
-`define LSL  4'd6  
-`define LSR  4'd7  
-`define DIV   4'd8  
-`define SLT   4'd9  
+`define AND  4'b0000  
+`define OR  4'b0001  
+`define NOR 4'b0010  
+`define ADD   4'b0011  
+`define SUB   4'b0100  
+`define XOR   4'b0101  
+`define LSL  4'b0110  
+`define LSR  4'b0111  
+`define DIV   4'b1000 
+`define SLT   4'b1001 
 
 //I-type
 
-`define LOAD   4'd10 
-`define STORE  4'd11 
-`define ADDI  4'd12 
-`define SUBI  4'd13 
-`define BEQ   4'd14 
+`define LOAD   4'b1010 
+`define STORE  4'b1011 
+`define ADDI  4'b1100 
+`define SUBI  4'b1101 
+`define BEQ   4'b1110 
 
 // J-Type
 
-`define B     4'd15 
+`define B     4'b1111 
 
 //modified start from https://github.com/robmarano/ece251_at_cooper/blob/mips_cpu-single_cycle/MIPS/mips_single_cycle_computer/mips_single_cycle.sv
 
@@ -31,25 +31,51 @@ module maindec(input logic [3:0] op,
                output logic memread, memwrite, 
                output logic branch, alusrc,
                output logic regdst, regwrite,
-               output logic [1:0] aluop, jump);
+               output logic [1:0] aluop,
+               output logic jump);
     
     logic [8:0] controls;
 
-    assign {regwrite, regdst, alusrc, branch, memwrite, memtoreg,jump,aluop} = controls;
+    assign {regwrite, regdst, alusrc, branch, memwrite, memread,jump,aluop} = controls;
     
+    
+
     always @*
+    
     case(op)
-        `AND | `OR | `NOR | `ADD | `SUB | `MOVZ | `LSL | `LSR | `DIV | `SLT : controls <= 9'b110000010; //R
-        `LOAD : controls <= 9'b101001000; //load
-        `STORE : controls <= 9'b001010000; // write
-        `ADDI | `SUBI : controls <= 9'101000000; // I
-        `BEQ : controls <= 9'b000100001; // CB
-        `B : controls <= 9'b000000100; // J-type
-        default: controls <= 9'bxxxxxxxxx;
-    endcase
-endmodule
 
-
-
-
+    //R-type
         
+        `AND: controls <= 9'b110000010; //R
+        `OR: controls <= 9'b110000010; //R
+        `NOR: controls <= 9'b110000010; //R
+        `ADD: controls <= 9'b110000010; //R
+        `SUB: controls <= 9'b110000010; //R
+        `XOR: controls <= 9'b110000010; //R
+        `LSL: controls <= 9'b110000010; //R
+        `LSR: controls <= 9'b110000010; //R
+        `DIV: controls <= 9'b110000010; //R
+        `SLT : controls <= 9'b110000010; //R
+
+    //load
+        `LOAD : controls <= 9'b101001000; //load
+
+    //write
+        `STORE : controls <= 9'b001010000; // write
+
+    //I-type
+        `ADDI : controls <= 9'b101000000; // I
+        `SUBI : controls <= 9'b101000000; // I
+
+    //CB
+        `BEQ : controls <= 9'b000100001; // CB
+
+    //J
+        `B : controls <= 9'b000000100; // J-type
+        default: controls <= 9'bxxxxxxxxx; 
+
+    endcase
+
+    
+
+endmodule
